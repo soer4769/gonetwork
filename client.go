0f14806e9c72594bb951b64d87f892c855ac6a8b
COHL7500 <cohl7500@gmail.com>
2022-12-28 14:44:32 +0100

    - Add comments - Add ShakeType enums

--- client.go
+++ client.go
@@ -1,72 +1,109 @@
 package main
 
 import (
-    "context"
-    "log"
-    "time"
-    "math/rand"
+	"context"
 	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"math/rand"
+	"time"
 
 	"github.com/gonetwork/proto"
 	"google.golang.org/grpc"
 )
 
+type ShakeType string
+
+const (
+	SYN ShakeType = "SYN"
+	ACK ShakeType = "ACK"
+)
+
+// Representation of the 3-way handshake.
 type Flags struct {
-    SYN, ACK, FIN bool
+	SYN, ACK, FIN bool
 }
 
+// Repesentation of a TCP package.
 type Pack struct {
-    SeqNum, AckNum uint32
-    Message string
-    Status Flags
+	SeqNum, AckNum uint32
+	Message        string
+	Status         Flags
 }
 
+// Function for sending message utilizing a pack.
 func SendMessage(c TCPHandshake.HandshakeClient, p Pack) (*TCPHandshake.TCPPack, error) {
-    r, err := c.ConnSend(context.Background(), &TCPHandshake.TCPPack{
-        SeqNum: p.SeqNum,
-        AckNum: p.AckNum,
-        Message: p.Message,
-        Status: &TCPHandshake.Flags{
-            SYN: p.Status.SYN,
-            ACK: p.Status.ACK,
-            FIN: p.Status.FIN,
-        },
-    })
 
-    if err != nil {
-        log.Fatalf("could not handshake: %v", err)
-    }
+	// Initiating the data to be sent corresponding to the pack.
+	r, err := c.ConnSend(context.Background(), &TCPHandshake.TCPPack{
+		SeqNum:  p.SeqNum,
+		AckNum:  p.AckNum,
+		Message: p.Message,
+		Status: &TCPHandshake.Flags{
+			SYN: p.Status.SYN,
+			ACK: p.Status.ACK,
+			FIN: p.Status.FIN,
+		},
+	})
 
-    return r, err
+	if err != nil {
+		log.Fatalf("could not handshake: %v", err)
+	}
+
+	// Send/return the message.
+	return r, err
 }
 
+func createPacket(msg ShakeType) Pack {
+
+	if msg == SYN {
+		return Pack{SeqNum: rand.Uint32(), Message: "SYN", Status: Flags{SYN: true}}
+	}
+
+	return Pack{SeqNum: rand.Uint32(), Message: "ACK", Status: Flags{ACK: true}}
+}
+
+// Perform handshake.
 func Shake(c TCPHandshake.HandshakeClient) {
-    log.Printf("Establishing Simulated TCP connection with server...")
+	log.Printf("Establishing Simulated TCP connection with server...")
 
-    syn := Pack{SeqNum: rand.Uint32(), Message: "SYN", Status: Flags{SYN: true}}
-    log.Printf("Sending message to server:\n\t%+v\n", syn)
+	// Send a packet to the server with SYN set to 1 (true); SYN packet.
+	syn := createPacket(SYN)
+	log.Printf("Sending message to server:\n\t%+v\n", syn)
 
-    r, err := SendMessage(c,syn)
-    if err != nil { return }
-    log.Printf("Recieved message from server:\n\t%+v\n", r)
+	r, err := SendMessage(c, syn)
+	if err != nil {
+		return
+	}
 
-    ack := Pack{AckNum: r.SeqNum+1, Message: "ACK", Status: Flags{ACK: true}}
-    log.Printf("sending message to server:\n\t%+v\n", ack)
+	// If no errors occured upon sending the message, server received the message.
+	log.Printf("Received message from server:\n\t%+v\n", r)
 
-    r, err = SendMessage(c,ack)
-    if err != nil { return }
+	// Server responds with SYN-ACK packet. Client sends back an ACK packet.
+	ack := createPacket(ACK)
+	log.Printf("sending message to server:\n\t%+v\n", ack)
 
-    log.Printf("Simulated TCP handshake successfully connected...")
+	r, err = SendMessage(c, ack)
+	if err != nil {
+		return
+	}
+
+	log.Printf("Simulated TCP handshake successfully connected...")
 }
 
 func main() {
-    rand.Seed(time.Now().UnixNano())
-    conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    c := TCPHandshake.NewHandshakeClient(conn)
 
+	// Changes the seed at start up so rand.Uint32 isn't the same every time.
+	rand.Seed(time.Now().UnixNano())
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	// New client.
+	c := TCPHandshake.NewHandshakeClient(conn)
+
+	// Commence a handshake.
 	Shake(c)
 	err = conn.Close()
 	if err != nil {
--- server.go
+++ server.go
@@ -1,64 +1,73 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net"
-    "time"
-    "math/rand"
+	"context"
+	"log"
+	"math/rand"
+	"net"
+	"time"
 
 	"github.com/gonetwork/proto"
 	"google.golang.org/grpc"
 )
 
 type Flags struct {
-    SYN, ACK, FIN bool
+	SYN, ACK, FIN bool
 }
 
 type Pack struct {
-    SeqNum, AckNum uint32
-    Message string
-    Status Flags
+	SeqNum, AckNum uint32
+	Message        string
+	Status         Flags
 }
 
 type server struct {
 	TCPHandshake.UnimplementedHandshakeServer
 }
 
-func (s *server) ConnSend(ctx context.Context, in *TCPHandshake.TCPPack) (*TCPHandshake.TCPPack, error) {
-    if in.Status.SYN {
-        log.Printf("New client trying to establish simulated TCP connection...")
-        log.Printf("Recieved message from client:\n\t%+v\n", in)
+// handle connection handshake for a client.
+func (s *server) ConnSend(_ context.Context, in *TCPHandshake.TCPPack) (*TCPHandshake.TCPPack, error) {
 
-        ack := Pack{SeqNum: rand.Uint32(), AckNum: in.SeqNum+1, Message: "SYN+ACK", Status: Flags{SYN: true, ACK: true}}
-        log.Printf("Sending response to client:\n\t%+v\n", ack)
+	// If status is SYN, a client is trying to establish a connection.
+	if in.Status.SYN {
+		log.Printf("New client trying to establish simulated TCP connection...")
+		log.Printf("Recieved message from client:\n\t%+v\n", in)
 
-        return &TCPHandshake.TCPPack{
-                 SeqNum: ack.SeqNum,
-                 AckNum: ack.AckNum,
-                 Message: ack.Message,
-                 Status: &TCPHandshake.Flags{
-                   SYN: ack.Status.SYN,
-                   ACK: ack.Status.ACK,
-                 },
-               }, nil
-    }
+		// Respond with a SYN-ACK to the client.
+		ack := Pack{SeqNum: rand.Uint32(), AckNum: in.SeqNum + 1, Message: "SYN+ACK", Status: Flags{SYN: true, ACK: true}}
+		log.Printf("Sending response to client:\n\t%+v\n", ack)
 
-    if in.Status.ACK {
-        log.Printf("Established simulated TCP connection succefully with client...")
-    }
+		// send the SYN-ACK.
+		return &TCPHandshake.TCPPack{
+			SeqNum:  ack.SeqNum,
+			AckNum:  ack.AckNum,
+			Message: ack.Message,
+			Status: &TCPHandshake.Flags{
+				SYN: ack.Status.SYN,
+				ACK: ack.Status.ACK,
+			},
+		}, nil
+	}
 
-    return &TCPHandshake.TCPPack{}, nil
+	// If client responds with ACK, connection successfully established.
+	if in.Status.ACK {
+		log.Printf("Established simulated TCP connection succefully with client...")
+	}
+
+	return &TCPHandshake.TCPPack{}, nil
 }
 
 func main() {
-    rand.Seed(time.Now().UnixNano())
-    lis, err := net.Listen("tcp", ":50051")
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    s := grpc.NewServer()
+	rand.Seed(time.Now().UnixNano())
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
+	// Initiate new server
+	s := grpc.NewServer()
+
+	// Register the server as a handshake server
 	TCPHandshake.RegisterHandshakeServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
